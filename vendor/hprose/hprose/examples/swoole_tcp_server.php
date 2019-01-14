5220df5e14d8762c73e9b1f1d26cb3ec0fe3b4f8
unknown <982435132@qq.com>
2019-01-14 09:29:48 +0800

    zhaobin

---
+++ vendor/hprose/hprose/examples/swoole_tcp_server.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('../Hprose.php');
+    function hello($name) {
+    	echo "Hello $name!";
+		return "Hello $name!";
+	}
+	function e() {
+		throw new Exception("I am Exception");
+	}
+	function ee() {
+		require("andot");
+	}
+	$server = new HproseSwooleServer("tcp://0.0.0.0:1504");
+	$server->setErrorTypes(E_ALL);
+	$server->setDebugEnabled();
+	$server->addFunction('hello');
+	$server->addFunctions(array('e', 'ee'));
+	$server->start();
+?>
