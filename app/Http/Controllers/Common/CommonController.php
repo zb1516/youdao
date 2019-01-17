4bd370aed2880c183aa0de4e1ab1a8dbf9acbac6
解翠萍 <xiecuiping@bj.gaosiedu.com>
2019-01-17 16:58:33 +0800

    '20190117-16:59-xcp'

--- app/Http/Controllers/Common/CommonController.php
+++ app/Http/Controllers/Common/CommonController.php
@@ -381,4 +381,27 @@ class CommonController extends BaseController
 
     }
 
+
+    /**
+     * 生成唯一标识
+     * @return string
+     */
+    public function guid() {
+        if (function_exists('com_create_guid')) {
+            return com_create_guid();
+        } else {
+            mt_srand((double)microtime()*10000);
+            $charid = strtoupper(md5(uniqid(rand(), true)));
+            $hyphen = chr(45);
+            $uuid   = chr(123)
+                .substr($charid, 0, 8).$hyphen
+                .substr($charid, 8, 4).$hyphen
+                .substr($charid,12, 4).$hyphen
+                .substr($charid,16, 4).$hyphen
+                .substr($charid,20,12)
+                .chr(125);
+            return $uuid;
+        }
+    }
+
 }
