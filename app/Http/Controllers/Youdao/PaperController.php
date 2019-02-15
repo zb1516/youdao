b4be30c4109edb21a0a9ef0bce5da420d326c18e
解翠萍 <xiecuiping@bj.gaosiedu.com>
2019-02-15 10:03:35 +0800

    '20190215-xcp'

--- app/Http/Controllers/Youdao/PaperController.php
+++ app/Http/Controllers/Youdao/PaperController.php
@@ -442,9 +442,8 @@ class PaperController extends BaseController
     {
         $code = 100;
         file_put_contents($_SERVER['DOCUMENT_ROOT'].'/paperExamined'.time().'.txt',json_encode($_POST));
-        return $code;
-        /*try{
-            $data = $request->post('data','');
+        try{
+            $data = $_REQUEST;
             if($data){
                 //$postData = json_decode($data,true);
                 //更新任务的有道审核结果，接收、处理时间
@@ -467,7 +466,7 @@ class PaperController extends BaseController
             return response()->json(['code'=>$code]);
         }catch (\Exception $e){
             return response()->json(['code'=>$code, 'errorMsg' => $e->getMessage()]);
-        }*/
+        }
     }
 
 
