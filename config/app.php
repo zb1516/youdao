582612247342ecf4ff10bb9e58fcbe64f63b4c91
unknown <982435132@qq.com>
2019-02-26 11:20:23 +0800

    zhaobin

--- config/app.php
+++ config/app.php
@@ -275,7 +275,7 @@ return [
         '4' => '已退回',
         '5' => '已关闭',
     ],
-    'OFFICE_DOCUMENT_BUCKET'=>'atf-document',
+    'OFFICE_DOCUMENT_BUCKET'=>'atf-uploads',
     'GRADE_NAME'=> [
         '1' => '一年级',
         '2' => '二年级',
