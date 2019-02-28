cb958a84dda653c06c9b5624199b6fbf8d8ea973
unknown <982435132@qq.com>
2019-02-28 16:12:16 +0800

    zhaobin

--- app/Http/Controllers/WxProgram/PaperController.php
+++ app/Http/Controllers/WxProgram/PaperController.php
@@ -496,6 +496,7 @@ class paperController extends Controller
             $searchArgs['isShare']=$request->input('isShare');
             $vipYoudaoExaminedModel=new VipYoudaoExamined();
             $paperExaminedInfo=$vipYoudaoExaminedModel->findOne(['task_id'=>$searchArgs['taskId']]);
+            dd($paperExaminedInfo);
             $paperInfo=KlibPaperClient::getPaperClient($paperExaminedInfo['paper_id']);
             $result = KlibQuestionClient::getQuestion($paperInfo['ques_ids']);
             $questions=[];
