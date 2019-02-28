db4c10f8d5e674684557cbe6a5edffca4228d235
解翠萍 <xiecuiping@bj.gaosiedu.com>
2019-02-28 17:26:52 +0800

    '20190228-17:26-xcp'

--- config/app.php
+++ config/app.php
@@ -253,7 +253,7 @@ return [
     ],
     'MIDDLE_TIME'=>strtotime(date('2018-11-07')),
     'AGENCY_UPLOAD_NUMBER'=>20,
-    'TEST_APP_KEY' => 'zhudytest123',
+    'TEST_APP_KEY' => 'aitifen@2009',//'zhudytest123',
     'TEST_APP_SECRET' => 'IoyvG6Zb98nEUA4nIGwkEPUXILBYgrGs',
     'TEST_YOUDAO_URL' => 'test-s4eu.youdao.com',
     'IMAGE_EXAMINED_STATUS'=> [
