034bea235604042a931049ffc2fb1acb0062b82d
unknown <982435132@qq.com>
2019-03-12 18:31:36 +0800

    zhaobin

--- app/Http/Controllers/Youdao/PaperController.php
+++ app/Http/Controllers/Youdao/PaperController.php
@@ -634,11 +634,7 @@ class PaperController extends BaseController
             $matches = array(array());
         }
         $spanStacks = array();
-        if((count($matches[0]) > 0)== true){
-            #dd(11111111111);
-        }
         if(is_array($matches) && $matches[0]){
-            dd($html);
             foreach($matches[0] as $key=>$match) {
                 if(false == stristr($match, '</span>')) {
                     array_push($spanStacks, array($key, $match));
