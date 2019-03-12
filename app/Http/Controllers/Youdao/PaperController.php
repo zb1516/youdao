86913871d6a9e41610437b2a9c917b9aec0eb024
unknown <982435132@qq.com>
2019-03-12 18:19:41 +0800

    zhaobin

--- app/Http/Controllers/Youdao/PaperController.php
+++ app/Http/Controllers/Youdao/PaperController.php
@@ -635,7 +635,7 @@ class PaperController extends BaseController
         }
 //        dd((is_array($matches) && !empty($matches[0])) !== false);
         $spanStacks = array();
-        if(false){
+        if((count($matches[0]) > 0)== false){
             dd(11111111111);
         }
         if((is_array($matches) && !empty($matches[0])) !== false){
