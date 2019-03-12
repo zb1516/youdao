88f2634ac86588ca08f381cea3aacdb7582f9029
unknown <982435132@qq.com>
2019-03-12 18:39:42 +0800

    zhaobin

--- app/Http/Controllers/Youdao/PaperController.php
+++ app/Http/Controllers/Youdao/PaperController.php
@@ -635,6 +635,7 @@ class PaperController extends BaseController
         }
         $spanStacks = array();
         if(is_array($matches) && $matches[0]){
+            var_dump($matches);
             foreach($matches[0] as $key=>$match) {
                 if(false == stristr($match, '</span>')) {
                     array_push($spanStacks, array($key, $match));
