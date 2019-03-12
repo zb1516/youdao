87d09bc1947092a3fab7a2a915e31e7be7af24de
unknown <982435132@qq.com>
2019-03-12 18:09:18 +0800

    zhaobin

--- app/Http/Controllers/Youdao/PaperController.php
+++ app/Http/Controllers/Youdao/PaperController.php
@@ -633,10 +633,10 @@ class PaperController extends BaseController
         if(false==$matches){
             $matches = array(array());
         }
-        var_dump(is_array($matches),$matches[0]);die;
+//        var_dump(is_array($matches),$matches[0]);die;
 //        dd((is_array($matches) && !empty($matches[0])) !== false);
         $spanStacks = array();
-        if((bool)(is_array($matches) && !empty($matches[0])) == true){
+        if(is_array($matches) && !empty($matches[0])){
             dd(11111111111);
         }
         if((is_array($matches) && !empty($matches[0])) !== false){
