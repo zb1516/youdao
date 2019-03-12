b0274fb882450776491b23b34ae58525881d97d1
unknown <982435132@qq.com>
2019-03-12 17:43:50 +0800

    'zhaobin'

--- app/Http/Controllers/Youdao/PaperController.php
+++ app/Http/Controllers/Youdao/PaperController.php
@@ -634,9 +634,8 @@ class PaperController extends BaseController
 //            $matches = array(array());
 //        }
         $spanStacks = array();
-
-        if(!empty($matches[0])){
-            var_dump(111);
+dd($matches);
+        if(!empty($matches)){
             foreach($matches[0] as $key=>$match) {
                 if(false == stristr($match, '</span>')) {
                     array_push($spanStacks, array($key, $match));
