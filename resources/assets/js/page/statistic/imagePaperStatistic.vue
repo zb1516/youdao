51d551441233f6b2c8a9e95433a0714e0d815569
unknown <982435132@qq.com>
2019-03-18 09:23:00 +0800

    zhaobin

--- resources/assets/js/page/statistic/imagePaperStatistic.vue
+++ resources/assets/js/page/statistic/imagePaperStatistic.vue
@@ -166,7 +166,7 @@
                 agencyValue:'',
                 optionsAgency:'',
                 imagePaperList: '',
-                pageSize: 10,
+                pageSize: 30,
                 currentPage:1,
                 curGrade:'',
                 _total:0,
@@ -287,7 +287,7 @@
                         callBack: function (currPage, pageSize) {
                             that.isSort = true;
                             that.currentPage = currPage;
-                            that.pageSize = 10;
+                            that.pageSize = 30;
                             that.doSearch();
                             //console.log('currPage:' + currPage + '     pageSize:' + that.pageSize);
 
