8f7d8c79b99d3ca1633f3a15a8873734868ed740
unknown <982435132@qq.com>
2019-03-19 10:25:40 +0800

    zhaobin

--- app/Http/Controllers/Youdao/PaperController.php
+++ app/Http/Controllers/Youdao/PaperController.php
@@ -91,7 +91,7 @@ class PaperController extends BaseController
                 $result = $common->getYoudaoTask($postUrl, $postData, 2);
 //                file_put_contents('/dev/shm/result.txt',$result);
                 $result = json_decode($result,true);
-                print_r($result);
+//                print_r($result);
                 if($result['code'] == 200){
                     if(isset($result['data']['questions']) && $result['data']['questions']){
                         foreach ($result['data']['questions'] as $key=>$ques){
