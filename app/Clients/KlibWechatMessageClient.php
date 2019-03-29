2c70bd6d92073af7572310d61a69053f2137ef48
解翠萍 <xiecuiping@bj.gaosiedu.com>
2019-03-29 10:38:13 +0800

    '20190329-10:38-xcp'

---
+++ app/Clients/KlibWechatMessageClient.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Clients;
+
+use Hprose\Http\Client;
+
+class KlibWechatMessageClient
+{
+    public static function sendWxTemplate($data , $type = 0)
+    {
+        try{
+            $client = new Client(env('KLIB_SERVICE_HOST'). '/klibService/quesService', false);
+            if($type == 0){
+                //失败通知
+                $result = $client->errorPatQues($data);
+            }else{
+                //成功通知
+                $result = $client->succPatQues($data);
+            }
+            file_put_contents($_SERVER['DOCUMENT_ROOT'].'/batchLog/weixin'.date('Ymd').'.txt', json_encode($result).PHP_EOL,FILE_APPEND);
+            return $result;
+        }catch (\Exception $e){
+            throw new \Exception($e->getMessage());
+        }
+    }
+}
